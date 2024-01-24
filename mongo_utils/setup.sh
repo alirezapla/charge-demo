a14f4877f2bec3e16b09c6c79709ab2d0e66cf60
alirezapla <hajlialireza@gmail.com>
2024-01-25 01:26:50 +0330

    feat: change cluster db docker compose file

--- mongo_utils/setup.sh
+++ mongo_utils/setup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-docker-compose exec config1 sh -c "mongosh  < /scripts/configserver.js"
+docker-compose -f docker-compose-db.yml exec config1 sh -c "mongosh  < /scripts/configserver.js"
 echo 'configed'
+echo "wait please"
 sleep 5
-docker-compose exec shard1a sh -c "mongosh  < /scripts/shard1.js"
-docker-compose exec shard2a sh -c "mongosh  < /scripts/shard2.js"
+docker-compose -f docker-compose-db.yml exec shard1a sh -c "mongosh  < /scripts/shard1.js"
+docker-compose -f docker-compose-db.yml exec shard2a sh -c "mongosh  < /scripts/shard2.js"
 echo 'shards initiliazed'
+echo "wait please"
 sleep 5
-docker-compose exec router sh -c "mongosh  < /scripts/router.js"
+docker-compose -f docker-compose-db.yml exec router sh -c "mongosh  < /scripts/router.js"
+echo "wait please"
 sleep 5
-docker-compose exec router sh -c "mongosh < /scripts/enable.js"
+docker-compose -f docker-compose-db.yml exec router sh -c "mongosh < /scripts/enable.js"
 echo 'ready'
